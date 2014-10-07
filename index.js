b156d0fa29c881b8ad64359bf2fe8e4f2963496b
Ben Wu <ben@chegg.com>
2014-10-06 17:51:00 -0700

    index.html

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 $(function() {
-    
+	$('#submit_button').click(function(){
+		var data = {}; 
+		$('#submit_form :input').each(function (){
+			var $el = $(this)
+			if($el.attr('name') !== undefined)
+				data[$el.attr('name')] = $el.val();
+		});
 
-    
+		saveStudent(data);
+		window.location.href="/invite.html";
+	});
 });
